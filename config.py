d79773d3644618a6e573e42e914a2312716ec316
Samuel Kalu <samkalu@ttu.edu>
2025-05-03 13:17:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
 # config.py
 
-MODEL_PATH = "CV_models/model.pt"  # YOLOv8 gesture model
-AUTH_USER_PATH = "FlaskAddUser/AuthUsers/"
-FLASK = ["10.181.114.19",  # IP address of web server
-          "Admin",          #Webserver username
-        "Prometheus",       #Webserver password
-    ]
+MODEL_PATH = "CV_models/YOLOv8nFinal.pt"  # updated YOLOv8n gesture model
+
 GESTURE_MAP = {
     0: "blue",
     1: "bright",
     2: "dim",
-    3: "down",
-    4: "cap",
-    5: "green",
-    6: "left",
-    7: "red",
-    8: "right",
-    9: "up"
+    3: "green",
+    4: "red",
 }
